a7c564aa6ad404e4247806b11943e8715469aab3
Mishev Deniz <denizmishev@yahoo.com>
2023-12-27 17:51:13 +0200

    updated enemyV1 logic so that each sprite is in a container, added health bars to enemies

--- src/scripts/game/EnemyV1.js
+++ src/scripts/game/EnemyV1.js
@@ -5,6 +5,7 @@ import { Shooting } from "./Shooting";
 export class EnemyV1 {
   constructor(enemyCount) {
     this.enemies = [];
+    this.enemyContainers = [];
     this.enemyCount = enemyCount;
     this.container = new PIXI.Container();
     this.shooting = new Shooting("enemyV1");
@@ -15,24 +16,45 @@ export class EnemyV1 {
 
   createEnemies() {
     const enemiesPerRow = 4;
-    const spacing = 120;
-    const rowSpacing = 60; // vertical spacing
-    const totalWidth = (enemiesPerRow - 1) * spacing;
+    const colSpacing = 120;
+    const rowSpacing = 80;
+    const totalWidth = (enemiesPerRow - 1) * colSpacing;
     const startX = (window.innerWidth - totalWidth) / 2 - 50;
     const startY = window.innerHeight * 0.05;
 
     for (let i = 0; i < this.enemyCount; i++) {
+      const enemyContainer = new PIXI.Container();
+
       const enemy = App.createSprite("enemyspaceshipv1");
       enemy.scale.set(0.3);
 
       const row = Math.floor(i / enemiesPerRow);
       const col = i % enemiesPerRow;
 
-      enemy.x = startX + col * spacing;
-      enemy.y = startY + row * rowSpacing;
-      this.container.addChild(enemy);
+      enemy.x = 0;
+      enemy.y = 0;
+      enemyContainer.addChild(enemy);
+
+      const healthBar = this.createHealthBar(enemy);
+      healthBar.y = enemy.height - 10;
+      enemyContainer.addChild(healthBar);
+
+      enemyContainer.x = startX + col * colSpacing;
+      enemyContainer.y = startY + row * rowSpacing;
+
+      this.container.addChild(enemyContainer);
       this.enemies.push(enemy);
+      this.enemyContainers.push(enemyContainer);
     }
+  }
+
+  createHealthBar(enemy) {
+    const healthBar = new PIXI.Graphics();
+    healthBar.beginFill(0xff1414);
+    healthBar.drawRect(20, 0, enemy.width - 40, 5);
+    healthBar.endFill();
+
+    return healthBar;
   }
 
   movementBehaviour() {
@@ -40,17 +62,18 @@ export class EnemyV1 {
     if (!this.direction) this.direction = 1; // determine the movement direction, 1 = right ; -1 = left
 
     const moveAmount = this.speed * this.direction;
-    this.enemies.forEach((enemy) => {
-      enemy.x += moveAmount;
+    this.enemyContainers.forEach((container) => {
+      container.x += moveAmount;
     });
 
-    // reverse direction on border collision
+    // check for border collision
 
-    const leftmostEnemy = this.enemies[0];
-    const rightmostEnemy = this.enemies[this.enemies.length - 1];
+    const leftmostContainer = this.enemyContainers[0];
+    const rightmostContainer =
+      this.enemyContainers[this.enemyContainers.length - 1];
     if (
-      leftmostEnemy.x < 0 ||
-      rightmostEnemy.x + rightmostEnemy.width > window.innerWidth
+      leftmostContainer.x < 0 ||
+      rightmostContainer.x + rightmostContainer.width > window.innerWidth
     ) {
       this.direction *= -1;
     }
@@ -70,16 +93,18 @@ export class EnemyV1 {
 
     if (
       now - this.lastShotTime > shotInterval &&
-      this.currentEnemyIndex < this.enemies.length
+      this.currentEnemyIndex < this.enemyContainers.length
     ) {
+      const enemyContainer = this.enemyContainers[this.currentEnemyIndex];
       const enemy = this.enemies[this.currentEnemyIndex];
-      const bulletX = enemy.x + enemy.width / 2;
-      const bulletY = enemy.y + enemy.height - 40;
+      const bulletX = enemyContainer.x + enemy.x + enemy.width / 2;
+      const bulletY = enemyContainer.y + enemy.y + enemy.height;
+
       this.shooting.shoot(bulletX, bulletY);
 
       this.lastShotTime = now;
       this.currentEnemyIndex =
-        (this.currentEnemyIndex + 1) % this.enemies.length;
+        (this.currentEnemyIndex + 1) % this.enemyContainers.length;
     }
   }
 
